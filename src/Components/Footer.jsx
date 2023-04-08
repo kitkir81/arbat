63811c5d3ddf624fe3821ae7b98b1afb054f0250
fixer400 <fixer400@yandex.ru>
2023-04-08 20:04:20 +0300

    Server and Layout
    added options for apache

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer(){
             <div>
                 <p className="footer__adress izax-Thin white">КавМинВоды, г.Пятигорск, ТРК Новый Арбат, ул.Октябрьская, д.17, офис 1</p>
                 <p className="footer__number izax-Thin white">+7 906 58 62 888</p>
-                <p className="footer__ID izax-Thin white">Самозаняты1й ИНН 261800824901</p>
+                <p className="footer__ID izax-Thin white">Самозанятый ИНН 261800824901</p>
             </div>
             <div className="footer__right">
                 <p className="footer__working-hours izax-Thin black">Режим работы Ежедневно с 09-00 до 20-00</p>
